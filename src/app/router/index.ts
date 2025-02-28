acc29c0b85573fab3db3e98de5b29f62d247a8a5
VUE Artem Kluev <mr.kluev9999@gmail.com>
2025-02-28 13:46:20 +0200

    test

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '@/components/pages/main/MainPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: import('@/components/pages/main/MainPage.vue'),
+      component: MainPage,
     },
     {
       path: '/shope',
